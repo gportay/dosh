8267c3236c23381f3963c8525eff60ba562b3010
Gaël PORTAY <gael.portay@savoirfairelinux.com>
2017-02-01 19:02:56 -0500

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# docker-scripts
-Some scripts on top of Docker
+# dsh(1)
+
+## NAME
+
+[dsh](dsh.1.adoc) - run a user shell in a new container with pwd bind mounted
+
+## DESCRIPTION
+
+[dsh](dsh) runs the _command_ process in a new container; using the current
+_user_, with _pwd_ bind mounted.
+
+## LINKS
+
+Check for the [man-page](dsh.1.adoc) and its [examples](dsh.1.adoc#examples)
+section.
+
+Also, here is an extra example that build the documentation
+
+	$ echo FROM ubuntu >Dockerfile
+	$ echo RUN apt-get update && apt-get install -y asciidoctor >>Dockerfile
+
+	$ cat Dockerfile
+	FROM ubuntu
+	RUN apt-get update && apt-get install -y asciidoctor
+
+	$ dsh -c asciidoctor -b manpage -o - dsh.1.adoc | gzip -c - >dsh.1.gz
+	Sending build context to Docker daemon 2.048 kB
+	Step 1 : FROM ubuntu
+	 ---> f49eec89601e
+	Step 2 : RUN apt-get update && apt-get install -y asciidoctor
+	 ---> Using cache
+	 ---> a21052a106d9
+	Step 3 : RUN groupadd --non-unique --gid 1000 gportay
+	 ---> Using cache
+	 ---> 9d5ca9684940
+	Step 4 : RUN useradd  --non-unique --gid 1000 --uid 1000 --create-home --home-dir /home/gportay --shell /bin/bash gportay
+	 ---> Using cache
+	 ---> ced062433e33
+	Successfully built ced062433e33
+
+	$ man ./dsh.1.gz
+
+Enjoy!
+
+## BUGS
+
+Report bugs at *https://github.com/gazoo74/docker-scripts/issues*
+
+## AUTHOR
+
+Written by Gaël PORTAY *gael.portay@savoirfairelinux.com*
+
+## COPYRIGHT
+
+Copyright (c) 2017 Gaël PORTAY
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the MIT License.
---
+++ tests.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+#
+# Copyright (c) 2017 Gaël PORTAY <gael.portay@savoirfairelinux.com>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the MIT License.
+#
+
+set -e
+
+PATH="$PWD:$PATH"
+
+test="Test without option with arguments"
+echo -e "\e[1mRunning $test...\e[0m"
+if          dsh "$@"  echo "one" "two" "three" | tee /dev/stderr | \
+   diff - <($SHELL    echo "one" "two" "three" | tee /dev/stderr )
+then
+	ok=$((ok+1))
+	echo -e "\e[1m$test: \e[32m[OK]\e[0m"
+else
+	ko=$((ko+1))
+	echo -e "\e[1m$test: \e[31m[KO]\e[0m"
+fi
+echo
+
+test="Test option -c without arguments"
+echo -e "\e[1mRunning $test...\e[0m"
+if          dsh "$@"  -c | tee /dev/stderr | \
+   diff - <($SHELL    -c | tee /dev/stderr )
+then
+	ok=$((ok+1))
+	echo -e "\e[1m$test: \e[32m[OK]\e[0m"
+else
+	ko=$((ko+1))
+	echo -e "\e[1m$test: \e[31m[KO]\e[0m"
+fi
+echo
+
+test="Test option -c without arguments"
+echo -e "\e[1mRunning $test...\e[0m"
+if          dsh "$@"  -c '' | tee /dev/stderr | \
+   diff - <($SHELL    -c '' | tee /dev/stderr )
+then
+	ok=$((ok+1))
+	echo -e "\e[1m$test: \e[32m[OK]\e[0m"
+else
+	ko=$((ko+1))
+	echo -e "\e[1m$test: \e[31m[KO]\e[0m"
+fi
+echo
+
+test="Test option -c command arguments"
+echo -e "\e[1mRunning $test...\e[0m"
+if          dsh "$@"  -c 'whoami; echo "$#" "$@" "one" "two" "three"' | tee /dev/stderr | \
+   diff - <($SHELL    -c 'whoami; echo "$#" "$@" "one" "two" "three"' | tee /dev/stderr )
+then
+	ok=$((ok+1))
+	echo -e "\e[1m$test: \e[32m[OK]\e[0m"
+else
+	ko=$((ko+1))
+	echo -e "\e[1m$test: \e[31m[KO]\e[0m"
+fi
+echo
+
+test="Test option -s without arguments"
+echo -e "\e[1mRunning $test...\e[0m"
+if          echo 'whoami; echo "$USER" "$0" "$#" "$@"' | dsh "$@" -s | tee /dev/stderr | \
+   diff - <(echo 'whoami; echo "$USER" "$0" "$#" "$@"' | $SHELL   -s | tee /dev/stderr )
+then
+	ok=$((ok+1))
+	echo -e "\e[1m$test: \e[32m[OK]\e[0m"
+else
+	ko=$((ko+1))
+	echo -e "\e[1m$test: \e[31m[KO]\e[0m"
+fi
+echo
+
+test="Test option -s one two three"
+echo -e "\e[1mRunning $test...\e[0m"
+if          echo 'whoami; echo "$USER" "$0" "$#" "$@"' | dsh "$@"  -s "one" "two" "three" | tee /dev/stderr | \
+   diff - <(echo 'whoami; echo "$USER" "$0" "$#" "$@"' | $SHELL    -s "one" "two" "three" )
+then
+	ok=$((ok+1))
+	echo -e "\e[1m$test: \e[32m[OK]\e[0m"
+else
+	ko=$((ko+1))
+	echo -e "\e[1m$test: \e[31m[KO]\e[0m"
+fi
+echo
+
+test="Test option -s \"one + two\" three"
+echo -e "\e[1mRunning $test...\e[0m"
+if          echo 'whoami; echo "$USER" "$0" "$#" "$@"' | dsh "$@"  -s "one + two" "three" | tee /dev/stderr | \
+   diff - <(echo 'whoami; echo "$USER" "$0" "$#" "$@"' | $SHELL    -s "one + two" "three" | tee /dev/stderr )
+then
+	# See FIXME in dsh
+	# ok=$((ok+1))
+	fix=$((fix+1))
+	echo -e "\e[1m$test: \e[34m[FIX]\e[0m"
+else
+	# See FIXME in dsh
+	# ko=$((ko+1))
+	bug=$((bug+1))
+	echo -e "\e[1m$test: \e[33m[BUG]\e[0m"
+fi
+echo
+
+if [ -n "$fix" ]; then
+	echo -e "\e[1m\e[33m$fix test(s) fixed!\e[0m" >&2
+fi
+
+if [ -n "$bug" ]; then
+	echo -e "\e[1mWarning: \e[33m$bug test(s) bug!\e[0m" >&2
+fi
+
+if [ -n "$ko" ]; then
+	echo -e "\e[1mError: \e[31m$ko test(s) failed!\e[0m" >&2
+	exit 1
+fi
+
+echo -e "\e[1m\e[32m$ok test(s) succeed!\e[0m"
