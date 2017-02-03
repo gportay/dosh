aa1ae33c0ac1cc73fb56b7bd4b1f5557cff0a674
Gaël PORTAY <gael.portay@savoirfairelinux.com>
2017-02-02 20:49:04 -0500

    Add --root/--no-user option to run as root

--- tests.sh
+++ tests.sh
@@ -64,8 +64,8 @@ echo
 
 test="Test option -s without arguments"
 echo -e "\e[1mRunning $test...\e[0m"
-if          echo 'whoami; echo "$USER" "$0" "$#" "$@"' | dsh "$@" -s | tee /dev/stderr | \
-   diff - <(echo 'whoami; echo "$USER" "$0" "$#" "$@"' | $SHELL   -s | tee /dev/stderr )
+if          echo 'whoami; echo "$0" "$#" "$@"' | dsh "$@" -s | tee /dev/stderr | \
+   diff - <(echo 'whoami; echo "$0" "$#" "$@"' | $SHELL   -s | tee /dev/stderr )
 then
 	ok=$((ok+1))
 	echo -e "\e[1m$test: \e[32m[OK]\e[0m"
@@ -77,8 +77,8 @@ echo
 
 test="Test option -s one two three"
 echo -e "\e[1mRunning $test...\e[0m"
-if          echo 'whoami; echo "$USER" "$0" "$#" "$@"' | dsh "$@"  -s "one" "two" "three" | tee /dev/stderr | \
-   diff - <(echo 'whoami; echo "$USER" "$0" "$#" "$@"' | $SHELL    -s "one" "two" "three" )
+if          echo 'whoami; echo "$0" "$#" "$@"' | dsh "$@"  -s "one" "two" "three" | tee /dev/stderr | \
+   diff - <(echo 'whoami; echo "$0" "$#" "$@"' | $SHELL    -s "one" "two" "three" )
 then
 	ok=$((ok+1))
 	echo -e "\e[1m$test: \e[32m[OK]\e[0m"
@@ -90,8 +90,103 @@ echo
 
 test="Test option -s \"one + two\" three"
 echo -e "\e[1mRunning $test...\e[0m"
-if          echo 'whoami; echo "$USER" "$0" "$#" "$@"' | dsh "$@"  -s "one + two" "three" | tee /dev/stderr | \
-   diff - <(echo 'whoami; echo "$USER" "$0" "$#" "$@"' | $SHELL    -s "one + two" "three" | tee /dev/stderr )
+if          echo 'whoami; echo "$0" "$#" "$@"' | dsh "$@"  -s "one + two" "three" | tee /dev/stderr | \
+   diff - <(echo 'whoami; echo "$0" "$#" "$@"' | $SHELL    -s "one + two" "three" | tee /dev/stderr )
+then
+	# See FIXME in dsh
+	# ok=$((ok+1))
+	fix=$((fix+1))
+	echo -e "\e[1m$test: \e[34m[FIX]\e[0m"
+else
+	# See FIXME in dsh
+	# ko=$((ko+1))
+	bug=$((bug+1))
+	echo -e "\e[1m$test: \e[33m[BUG]\e[0m"
+fi
+echo
+
+test="Test option --root with arguments"
+echo -e "\e[1mRunning $test...\e[0m"
+if                      dsh "$@"  --root echo "one" "two" "three" | tee /dev/stderr | \
+   diff - <(fakeroot -- $SHELL           echo "one" "two" "three" | tee /dev/stderr )
+then
+	ok=$((ok+1))
+	echo -e "\e[1m$test: \e[32m[OK]\e[0m"
+else
+	ko=$((ko+1))
+	echo -e "\e[1m$test: \e[31m[KO]\e[0m"
+fi
+echo
+
+test="Test option --root and -c without arguments"
+echo -e "\e[1mRunning $test...\e[0m"
+if                      dsh "$@"  --root -c | tee /dev/stderr | \
+   diff - <(fakeroot -- $SHELL           -c | tee /dev/stderr )
+then
+	ok=$((ok+1))
+	echo -e "\e[1m$test: \e[32m[OK]\e[0m"
+else
+	ko=$((ko+1))
+	echo -e "\e[1m$test: \e[31m[KO]\e[0m"
+fi
+echo
+
+test="Test option --root and -c with empty arguments"
+echo -e "\e[1mRunning $test...\e[0m"
+if                      dsh "$@"  --root -c '' | tee /dev/stderr | \
+   diff - <(fakeroot -- $SHELL           -c '' | tee /dev/stderr )
+then
+	ok=$((ok+1))
+	echo -e "\e[1m$test: \e[32m[OK]\e[0m"
+else
+	ko=$((ko+1))
+	echo -e "\e[1m$test: \e[31m[KO]\e[0m"
+fi
+echo
+
+test="Test option --root and -c command arguments"
+echo -e "\e[1mRunning $test...\e[0m"
+if                      dsh "$@"  --root -c 'whoami; echo "$#" "$@" "one" "two" "three"' | tee /dev/stderr | \
+   diff - <(fakeroot -- $SHELL           -c 'whoami; echo "$#" "$@" "one" "two" "three"' | tee /dev/stderr )
+then
+	ok=$((ok+1))
+	echo -e "\e[1m$test: \e[32m[OK]\e[0m"
+else
+	ko=$((ko+1))
+	echo -e "\e[1m$test: \e[31m[KO]\e[0m"
+fi
+echo
+
+test="Test option --root and -s without arguments"
+echo -e "\e[1mRunning $test...\e[0m"
+if          echo 'whoami; echo "$0" "$#" "$@"' |             dsh "$@"  --root -s | tee /dev/stderr | \
+   diff - <(echo 'whoami; echo "$0" "$#" "$@"' | fakeroot -- $SHELL           -s | tee /dev/stderr )
+then
+	ok=$((ok+1))
+	echo -e "\e[1m$test: \e[32m[OK]\e[0m"
+else
+	ko=$((ko+1))
+	echo -e "\e[1m$test: \e[31m[KO]\e[0m"
+fi
+echo
+
+test="Test option --root and -s one two three"
+echo -e "\e[1mRunning $test...\e[0m"
+if          echo 'whoami; echo "$0" "$#" "$@"' |             dsh "$@"  --root -s "one" "two" "three" | tee /dev/stderr | \
+   diff - <(echo 'whoami; echo "$0" "$#" "$@"' | fakeroot -- $SHELL           -s "one" "two" "three" | tee /dev/stderr )
+then
+	ok=$((ok+1))
+	echo -e "\e[1m$test: \e[32m[OK]\e[0m"
+else
+	ko=$((ko+1))
+	echo -e "\e[1m$test: \e[31m[KO]\e[0m"
+fi
+echo
+
+test="Test option --root and -s \"one + two\" three"
+echo -e "\e[1mRunning $test...\e[0m"
+if          echo 'whoami; echo "$0" "$#" "$@"' |             dsh "$@"  --root -s "one + two" "three" | tee /dev/stderr | \
+   diff - <(echo 'whoami; echo "$0" "$#" "$@"' | fakeroot -- $SHELL           -s "one + two" "three" | tee /dev/stderr )
 then
 	# See FIXME in dsh
 	# ok=$((ok+1))
