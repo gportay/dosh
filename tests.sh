f3bef3010c2f9c91af8da2af9779d1f11d95e7c3
Gaël PORTAY <gael.portay@savoirfairelinux.com>
2017-02-02 19:24:40 -0500

    Add support for shebang with arguments

--- tests.sh
+++ tests.sh
@@ -144,6 +144,19 @@ else
 fi
 echo
 
+test="Test shebang with arguments"
+echo -e "\e[1mRunning $test...\e[0m"
+if ./shebang-fedora.dsh | tee /dev/stderr | \
+   grep -q 'PRETTY_NAME="Fedora 25 (Twenty Five)'
+then
+	ok=$((ok+1))
+	echo -e "\e[1m$test: \e[32m[OK]\e[0m"
+else
+	ko=$((ko+1))
+	echo -e "\e[1m$test: \e[31m[KO]\e[0m"
+fi
+echo
+
 if [ -n "$fix" ]; then
 	echo -e "\e[1m\e[33m$fix test(s) fixed!\e[0m" >&2
 fi
