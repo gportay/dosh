gportay/dosh

commit 97fd233f4a51ed51ef3ade0d99b49ae5f3b09321

Add DOSHELL environment variable
This variable sets the shell to run in the docker image.

tests.sh
@@ -58,7 +58,7 @@ trap result 0
 
 run "dosh: Test without option with arguments"
 if          dosh "$@"  echo "one" "two" "three" | tee /dev/stderr | \
-   diff - <($SHELL     echo "one" "two" "three" | tee /dev/stderr )
+   diff - <(/bin/sh    echo "one" "two" "three" | tee /dev/stderr )
 then
 	ok
 else
@@ -68,7 +68,7 @@ echo
 
 run "dosh: Test option -c without arguments"
 if          dosh "$@"  -c | tee /dev/stderr | \
-   diff - <($SHELL     -c | tee /dev/stderr )
+   diff - <(/bin/sh    -c | tee /dev/stderr )
 then
 	ok
 else
@@ -78,7 +78,7 @@ echo
 
 run "dosh: Test option -c with empty argument"
 if          dosh "$@"  -c '' | tee /dev/stderr | \
-   diff - <($SHELL     -c '' | tee /dev/stderr )
+   diff - <(/bin/sh    -c '' | tee /dev/stderr )
 then
 	ok
 else
@@ -88,7 +88,7 @@ echo
 
 run "dosh: Test option -c command arguments"
 if          dosh "$@"  -c 'whoami; echo "$#" "$@"' | tee /dev/stderr | \
-   diff - <($SHELL     -c 'whoami; echo "$#" "$@"' | tee /dev/stderr )
+   diff - <(/bin/sh    -c 'whoami; echo "$#" "$@"' | tee /dev/stderr )
 then
 	ok
 else
@@ -98,7 +98,7 @@ echo
 
 run "dosh: Test option -s without arguments"
 if          echo 'whoami; echo "$0" "$#" "$@"' | dosh "$@" -s | tee /dev/stderr | \
-   diff - <(echo 'whoami; echo "$0" "$#" "$@"' | $SHELL    -s | tee /dev/stderr )
+   diff - <(echo 'whoami; echo "$0" "$#" "$@"' | /bin/sh   -s | tee /dev/stderr )
 then
 	ok
 else
@@ -108,7 +108,7 @@ echo
 
 run "dosh: Test option -s one two three"
 if          echo 'whoami; echo "$0" "$#" "$@"' | dosh "$@"  -s "one" "two" "three" | tee /dev/stderr | \
-   diff - <(echo 'whoami; echo "$0" "$#" "$@"' | $SHELL     -s "one" "two" "three" | tee /dev/stderr )
+   diff - <(echo 'whoami; echo "$0" "$#" "$@"' | /bin/sh    -s "one" "two" "three" | tee /dev/stderr )
 then
 	ok
 else
@@ -118,7 +118,7 @@ echo
 
 run "dosh: Test option -s one\ +\ two three"
 if          echo 'whoami; echo "$0" "$#" "$@"' | dosh "$@"  -s one\ +\ two three | tee /dev/stderr | \
-   diff - <(echo 'whoami; echo "$0" "$#" "$@"' | $SHELL     -s one\ +\ two three | tee /dev/stderr )
+   diff - <(echo 'whoami; echo "$0" "$#" "$@"' | /bin/sh    -s one\ +\ two three | tee /dev/stderr )
 then
 	ok
 else
@@ -128,7 +128,7 @@ echo
 
 run "dosh: Test option -s \"one + two\" three"
 if          echo 'whoami; echo "$0" "$#" "$@"' | dosh "$@"  -s "one + two" "three" | tee /dev/stderr | \
-   diff - <(echo 'whoami; echo "$0" "$#" "$@"' | $SHELL     -s "one + two" "three" | tee /dev/stderr )
+   diff - <(echo 'whoami; echo "$0" "$#" "$@"' | /bin/sh    -s "one + two" "three" | tee /dev/stderr )
 then
 	ok
 else
@@ -138,7 +138,7 @@ echo
 
 run "dosh: Test option --root with arguments"
 if                      dosh "$@"  --root echo "one" "two" "three" | tee /dev/stderr | \
-   diff - <(fakeroot -- $SHELL            echo "one" "two" "three" | tee /dev/stderr )
+   diff - <(fakeroot -- /bin/sh           echo "one" "two" "three" | tee /dev/stderr )
 then
 	ok
 else
@@ -148,7 +148,7 @@ echo
 
 run "dosh: Test option --root and -c without arguments"
 if                      dosh "$@"  --root -c | tee /dev/stderr | \
-   diff - <(fakeroot -- $SHELL            -c | tee /dev/stderr )
+   diff - <(fakeroot -- /bin/sh           -c | tee /dev/stderr )
 then
 	ok
 else
@@ -158,7 +158,7 @@ echo
 
 run "dosh: Test option --root and -c with empty arguments"
 if                      dosh "$@"  --root -c '' | tee /dev/stderr | \
-   diff - <(fakeroot -- $SHELL            -c '' | tee /dev/stderr )
+   diff - <(fakeroot -- /bin/sh           -c '' | tee /dev/stderr )
 then
 	ok
 else
@@ -168,7 +168,7 @@ echo
 
 run "dosh: Test option --root and -c command arguments"
 if                      dosh "$@"  --root -c 'whoami; echo "$#" "$@" "one" "two" "three"' | tee /dev/stderr | \
-   diff - <(fakeroot -- $SHELL            -c 'whoami; echo "$#" "$@" "one" "two" "three"' | tee /dev/stderr )
+   diff - <(fakeroot -- /bin/sh           -c 'whoami; echo "$#" "$@" "one" "two" "three"' | tee /dev/stderr )
 then
 	ok
 else
@@ -178,7 +178,7 @@ echo
 
 run "dosh: Test option --root and -s without arguments"
 if          echo 'whoami; echo "$0" "$#" "$@"' |             dosh "$@"  --root -s | tee /dev/stderr | \
-   diff - <(echo 'whoami; echo "$0" "$#" "$@"' | fakeroot -- $SHELL            -s | tee /dev/stderr )
+   diff - <(echo 'whoami; echo "$0" "$#" "$@"' | fakeroot -- /bin/sh           -s | tee /dev/stderr )
 then
 	ok
 else
@@ -188,7 +188,7 @@ echo
 
 run "dosh: Test option --root and -s one two three"
 if          echo 'whoami; echo "$0" "$#" "$@"' |             dosh "$@"  --root -s "one" "two" "three" | tee /dev/stderr | \
-   diff - <(echo 'whoami; echo "$0" "$#" "$@"' | fakeroot -- $SHELL            -s "one" "two" "three" | tee /dev/stderr )
+   diff - <(echo 'whoami; echo "$0" "$#" "$@"' | fakeroot -- /bin/sh           -s "one" "two" "three" | tee /dev/stderr )
 then
 	ok
 else
@@ -198,7 +198,7 @@ echo
 
 run "dosh: Test option --root and -s one\ +\ two three"
 if          echo 'whoami; echo "$0" "$#" "$@"' |             dosh "$@"  --root -s one\ +\ two three | tee /dev/stderr | \
-   diff - <(echo 'whoami; echo "$0" "$#" "$@"' | fakeroot -- $SHELL            -s one\ +\ two three | tee /dev/stderr )
+   diff - <(echo 'whoami; echo "$0" "$#" "$@"' | fakeroot -- /bin/sh           -s one\ +\ two three | tee /dev/stderr )
 then
 	ok
 else
@@ -208,7 +208,7 @@ echo
 
 run "dosh: Test option --root and -s \"one + two\" three"
 if          echo 'whoami; echo "$0" "$#" "$@"' |             dosh "$@"  --root -s "one + two" "three" | tee /dev/stderr | \
-   diff - <(echo 'whoami; echo "$0" "$#" "$@"' | fakeroot -- $SHELL            -s "one + two" "three" | tee /dev/stderr )
+   diff - <(echo 'whoami; echo "$0" "$#" "$@"' | fakeroot -- /bin/sh           -s "one + two" "three" | tee /dev/stderr )
 then
 	ok
 else
@@ -260,7 +260,7 @@ echo
 
 run "dosh: Test option --home"
 if          echo 'pwd; cd ; pwd' |             dosh "$@"  --home -s | tee /dev/stderr | \
-   diff - <(echo 'pwd; cd ; pwd' | fakeroot -- $SHELL            -s | tee /dev/stderr )
+   diff - <(echo 'pwd; cd ; pwd' | fakeroot -- /bin/sh           -s | tee /dev/stderr )
 then
 	ok
 else
@@ -268,7 +268,7 @@ else
 fi
 echo
 
-run "dosh: Test option --sh"
+run "dosh: Test deprecated option --sh"
 if          echo 'echo $0' | dosh "$@"  --sh -s | tee /dev/stderr | \
    diff - <(echo 'echo $0' | /bin/sh         -s | tee /dev/stderr )
 then
@@ -290,7 +290,7 @@ echo
 
 run "dosh: Test default shell"
 if          echo 'echo $0' | dosh "$@"  -s | tee /dev/stderr | \
-   diff - <(echo 'echo $0' | $SHELL     -s | tee /dev/stderr )
+   diff - <(echo 'echo $0' | /bin/sh    -s | tee /dev/stderr )
 then
 	ok
 else
@@ -332,7 +332,7 @@ fi
 echo
 
 run "dosh: Test with a busybox based distro (/bin/ash + adduser/addgroup)"
-if SHELL=/bin/ash dosh -F Dockerfile.alpine --build "$@" -c "cat /etc/os*release"
+if DOSHELL=/bin/ash dosh -F Dockerfile.alpine --build "$@" -c "cat /etc/os*release"
 then
 	ok
 else

dosh
@@ -43,7 +43,7 @@ Options:
             --root              Run as root.
             --home              Bind mount home directory.
             --sh                Set /bin/sh as default shell.
-                                Equivalent to set SHELL=/bin/sh,
+                                Equivalent to set DOSHELL=/bin/sh,
                                            or set --shell /bin/sh.
                                 This option is deprecated; use --shell instead.
             --shell SHELL       Set shell interpretor.
@@ -54,6 +54,10 @@ Options:
       -v or --verbose           Turn on verbose mode.
       -V or --version           Print version.
       -h or --help              Print usage.
+
+Environment variables:
+      DOSHELL                   The full pathname to the shell to run in docker
+                                image.
 EOF
 }
 
@@ -67,8 +71,8 @@ docker_build() {
 
 RUN groupadd --non-unique --gid ${GROUPS[0]} $USER \
  || addgroup -g ${GROUPS[0]} $USER
-RUN useradd  --non-unique --gid ${GROUPS[0]} --uid $UID --create-home --home-dir /home/$USER --shell $SHELL $USER \
- || adduser  -G $USER -u $UID -h /home/$USER -s $SHELL -D $USER
+RUN useradd  --non-unique --gid ${GROUPS[0]} --uid $UID --create-home --home-dir /home/$USER --shell /bin/sh $USER \
+ || adduser  -G $USER -u $UID -h /home/$USER -s /bin/sh -D $USER
 EOF
 }
 
@@ -84,6 +88,7 @@ build=false
 detach=false
 exec=false
 buildopts="--quiet"
+DOSHELL="${DOSHELL:-/bin/sh}"
 while [ "$#" -ne 0 ]; do
 	if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
 		usage
@@ -103,10 +108,10 @@ while [ "$#" -ne 0 ]; do
 		dwd="$PWD"
 		home=true
 	elif [ "$1" = "--sh" ]; then
-		SHELL="/bin/sh"
+		DOSHELL="/bin/sh"
 	elif [ "$1" = "--shell" ]; then
 		shift
-		SHELL="$1"
+		DOSHELL="$1"
 	elif [ "$1" = "--build" ]; then
 		build=true
 	elif [ "$1" = "--detach" ]; then
@@ -188,13 +193,13 @@ fi
 
 # Read command from stdin
 if $opt_s; then
-	set -- "$SHELL" -c "cd $dwd && $SHELL -s $(eval echo "${@// /\\\\ }")"
+	set -- "$DOSHELL" -c "cd $dwd && $DOSHELL -s $(eval echo "${@// /\\\\ }")"
 # Run command from arguments
 elif $opt_c; then
-	set -- "$SHELL" -c "cd $dwd && $SHELL -c '$*'"
+	set -- "$DOSHELL" -c "cd $dwd && $DOSHELL -c '$*'"
 # No command or invalid
 else
-	set -- "$SHELL" -c "cd $dwd && $SHELL $*"
+	set -- "$DOSHELL" -c "cd $dwd && $DOSHELL $*"
 fi
 
 # Execute in existing container

commit a28637a004db82a2c1a2a60604f129f4d00a55fb

tests.sh: fix broken tests
Tests are broken since the DOSHELL variable has been introduced in commit

	97fd233 Add DOSHELL environment variable

tests.sh
@@ -56,6 +56,8 @@ result() {
 PATH="$PWD:$PATH"
 trap result 0
 
+export -n DOSHELL
+
 run "dosh: Test without option with arguments"
 if          dosh "$@"  echo "one" "two" "three" | tee /dev/stderr | \
    diff - <(/bin/sh    echo "one" "two" "three" | tee /dev/stderr )