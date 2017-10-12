ff0b6655b749fcbbca4c78fca1a946ede8c66069
Gaël PORTAY <gael.portay@savoirfairelinux.com>
2017-10-11 22:30:04 -0400

    Improve grml zsh integration

--- Dockerfile.zsh
+++ Dockerfile.zsh
@@ -1,3 +1,5 @@
 FROM ubuntu:16.04
 RUN apt-get update && apt-get install -y asciidoctor
 RUN apt-get update && apt-get install -y zsh
+RUN apt-get update && apt-get install -y wget
+RUN wget -O /etc/zsh/zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
