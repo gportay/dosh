7f9087a77695b2f9fa6fb0c2e9812c0d69d0753e
Gaël PORTAY <gael.portay@savoirfairelinux.com>
2018-09-27 20:21:00 -0400

    Remove deprecated --sh option
    This option is deprecated since 3cf3bf3 (Add --shell SHELL option).

--- tests.sh
+++ tests.sh
@@ -336,16 +336,6 @@ else
 fi
 echo
 
-run "dosh: Test deprecated option --sh"
-if          echo 'echo $0' | dosh "$@"  --sh -s | tee /dev/stderr | \
-   diff - <(echo 'echo $0' | /bin/sh         -s | tee /dev/stderr )
-then
-	ok
-else
-	ko
-fi
-echo
-
 run "dosh: Test option --shell /bin/dash"
 if          echo 'echo $0' | dosh "$@"  --shell /bin/dash -s | tee /dev/stderr | \
    diff - <(echo '/bin/dash'                                 | tee /dev/stderr )
