05f4aa722c81fade41697d533962ed6b717d79d5
Gaël PORTAY <gael.portay@collabora.com>
2019-06-04 21:36:29 -0400

    Remove e-mail from headers

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2017-2019 Gaël PORTAY <gael.portay@gmail.com>
+# Copyright (c) 2017-2019 Gaël PORTAY
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the MIT License.
--- tests.sh
+++ tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2017-2019 Gaël PORTAY <gael.portay@gmail.com>
+# Copyright (c) 2017-2019 Gaël PORTAY
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the MIT License.
