f0450287fdd416df62890c99f40156d57526012c
Gaël PORTAY <gael.portay@gmail.com>
2025-07-16 20:19:25 +0200

    Add make-rpm.sh

---
+++ make-rpm.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env -S DOSH_DOCKERFILE=Dockerfile.rpm DOSH_DOCKER_RUN_EXTRA_OPTS="--volume ${PWD}/rpmbuild:${HOME}/rpmbuild --volume ${PWD}/dosh.spec:${HOME}/rpmbuild/SPECS/dosh.spec" dosh
+set -e
+rpmdev-setuptree
+cd ~/rpmbuild/SPECS
+rpmbuild --undefine=dist --undefine=_disable_source_fetch -ba dosh.spec "$@"
+cp ~/rpmbuild/SRPMS/*.src.rpm ~/rpmbuild/RPMS/*/*.rpm "$OLDPWD"
+rpmlint *.rpm
