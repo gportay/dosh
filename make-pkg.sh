87e94aed436bbe55b1e39c438fe21e81c4136dfc
Gaël PORTAY <gael.portay@gmail.com>
2025-07-23 21:46:15 +0200

    make-pkg.sh: shellcheck PKGBUILD

--- make-pkg.sh
+++ make-pkg.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env -S DOSH_DOCKERFILE=Dockerfile.pkg dosh
 set -e
 makepkg --force --skipchecksums "$@"
+shellcheck --shell=bash --exclude=SC2034,SC2154,SC2164 PKGBUILD
 namcap PKGBUILD* *.pkg.tar*
