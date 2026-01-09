gportay/domake

Merge platform packages together
Package transition[1].

	case	Package (transition) situation				Flags for new A package		Flags for new B package
	#5	Rename: only A existed; move everything from A into B;	Depends: B			Breaks: A (<<2)
		make A a transitional package without real contents. 					Replaces: A (<<2)
		See RenamingPackages[2] for an in-depth explanation.					optional* -- Provides: A

*: Provides are needed only when there are some packages which depend on A.

This merges the linux platform packages to linux-platforms, and creates
transition packages.

[1]: https://wiki.debian.org/PackageTransition
[2]: https://wiki.debian.org/RenamingPackages

Makefile
@@ -53,6 +53,17 @@ install-docker-cli-plugin: DOCKERLIBDIR ?= $(PREFIX)/lib/docker
 install-docker-cli-plugin:
 	install -D -m 755 support/docker-make $(DESTDIR)$(DOCKERLIBDIR)/cli-plugins/docker-make
 
+.PHONY: install-linux-platforms
+install-linux-platforms: install-linux-amd64-domake
+install-linux-platforms: install-linux-arm64-domake
+install-linux-platforms: install-linux-arm-domake
+install-linux-platforms: install-linux-arm-v6-domake
+install-linux-platforms: install-linux-arm-v7-domake
+install-linux-platforms: install-linux-ppc64le-domake
+install-linux-platforms: install-linux-riscv64-domake
+install-linux-platforms: install-linux-s390x-domake
+install-linux-platforms:
+
 install-linux-amd64-domake:
 install-linux-arm64-domake:
 install-linux-arm-domake:
@@ -108,6 +119,7 @@ user-install:
 user-install-doc:
 user-install-bash-completion:
 user-install-docker-cli-plugin:
+user-install-linux-platforms:
 user-install-linux-amd64-domake:
 user-install-linux-arm64-domake:
 user-install-linux-arm-domake:

PKGBUILD
@@ -2,12 +2,7 @@
 
 pkgname=(domake
 	 domake-docker-make
-	 domake-linux-amd64
-	 domake-linux-arm64
-	 domake-linux-arm
-	 domake-linux-ppc64le
-	 domake-linux-riscv64
-	 domake-linux-s390x)
+	 domake-linux-platforms)
 pkgver=3
 pkgrel=1
 pkgdesc='Docker make'
@@ -50,56 +45,11 @@ package_domake-docker-make() {
 	install -D -m 644 LICENSE "$pkgdir/usr/share/licenses/domake-docker-make/LICENSE"
 }
 
-package_domake-linux-amd64() {
-	pkgdesc='Docker shell for linux/amd64 platform'
+package_domake-linux-platforms() {
+	pkgdesc='Docker shell for linux platforms'
 	depends+=(domake)
 
 	cd "domake-$pkgver"
-	make DESTDIR="$pkgdir" PREFIX="/usr" install-linux-amd64-domake
-	install -D -m 644 LICENSE "$pkgdir/usr/share/licenses/domake-linux-amd64/LICENSE"
-}
-
-package_domake-linux-arm64() {
-	pkgdesc='Docker shell for linux/arm64 platform'
-	depends+=(domake)
-
-	cd "domake-$pkgver"
-	make DESTDIR="$pkgdir" PREFIX="/usr" install-linux-arm64-domake
-	install -D -m 644 LICENSE "$pkgdir/usr/share/licenses/domake-linux-arm64/LICENSE"
-}
-
-package_domake-linux-arm() {
-	pkgdesc='Docker shell for linux/arm platform'
-	depends+=(domake)
-
-	cd "domake-$pkgver"
-	make DESTDIR="$pkgdir" PREFIX="/usr" install-linux-arm-domake install-linux-arm-v6-domake install-linux-arm-v7-domake
-	install -D -m 644 LICENSE "$pkgdir/usr/share/licenses/domake-linux-arm/LICENSE"
-}
-
-package_domake-linux-ppc64le() {
-	pkgdesc='Docker shell for linux/ppc64le platform'
-	depends+=(domake)
-
-	cd "domake-$pkgver"
-	make DESTDIR="$pkgdir" PREFIX="/usr" install-linux-ppc64le-domake
-	install -D -m 644 LICENSE "$pkgdir/usr/share/licenses/domake-linux-ppc64le/LICENSE"
-}
-
-package_domake-linux-riscv64() {
-	pkgdesc='Docker shell for linux/riscv64 platform'
-	depends+=(domake)
-
-	cd "domake-$pkgver"
-	make DESTDIR="$pkgdir" PREFIX="/usr" install-linux-riscv64-domake
-	install -D -m 644 LICENSE "$pkgdir/usr/share/licenses/domake-linux-riscv64/LICENSE"
-}
-
-package_domake-linux-s390x() {
-	pkgdesc='Docker shell for linux/s390x platform'
-	depends+=(domake)
-
-	cd "domake-$pkgver"
-	make DESTDIR="$pkgdir" PREFIX="/usr" install-linux-s390x-domake
-	install -D -m 644 LICENSE "$pkgdir/usr/share/licenses/domake-linux-s390x/LICENSE"
+	make DESTDIR="$pkgdir" PREFIX="/usr" install-linux-platforms
+	install -D -m 644 LICENSE "$pkgdir/usr/share/licenses/domake-linux-platforms/LICENSE"
 }

gportay/dosh

Makefile
@@ -100,6 +100,17 @@ install-docker-cli-plugin-%: DOCKERLIBDIR ?= $(PREFIX)/lib/docker
 install-docker-cli-plugin-%:
 	ln -sf docker-shell $(DESTDIR)$(DOCKERLIBDIR)/cli-plugins/docker-$*
 
+.PHONY: install-linux-platforms
+install-linux-platforms: install-linux-amd64-dosh
+install-linux-platforms: install-linux-arm64-dosh
+install-linux-platforms: install-linux-arm-dosh
+install-linux-platforms: install-linux-arm-v6-dosh
+install-linux-platforms: install-linux-arm-v7-dosh
+install-linux-platforms: install-linux-ppc64le-dosh
+install-linux-platforms: install-linux-riscv64-dosh
+install-linux-platforms: install-linux-s390x-dosh
+install-linux-platforms:
+
 install-linux-amd64-dosh:
 install-linux-arm64-dosh:
 install-linux-arm-dosh:
@@ -182,6 +193,7 @@ user-install-docker-cli-plugin:
 user-install-docker-cli-plugin-sh:
 user-install-docker-cli-plugin-bash:
 user-install-docker-cli-plugin-zsh:
+user-install-linux-platforms:
 user-install-linux-amd64-dosh:
 user-install-linux-arm64-dosh:
 user-install-linux-arm-dosh:

PKGBUILD
@@ -2,12 +2,7 @@
 
 pkgname=(dosh
 	 dosh-docker-shell
-	 dosh-linux-amd64
-	 dosh-linux-arm64
-	 dosh-linux-arm
-	 dosh-linux-ppc64le
-	 dosh-linux-riscv64
-	 dosh-linux-s390x
+	 dosh-linux-platforms
 	 dosh-posh
 	 dosh-cqfd
 	 dosh-docker-cqfd)
@@ -55,58 +50,13 @@ package_dosh-docker-shell() {
 	install -D -m 644 LICENSE "$pkgdir/usr/share/licenses/dosh-docker-shell/LICENSE"
 }
 
-package_dosh-linux-amd64() {
-	pkgdesc='Docker shell for linux/amd64 platform'
+package_dosh-linux-platforms() {
+	pkgdesc='Docker shell for linux platforms'
 	depends+=(dosh)
 
 	cd "dosh-$pkgver"
-	make DESTDIR="$pkgdir" PREFIX="/usr" install-linux-amd64-dosh
-	install -D -m 644 LICENSE "$pkgdir/usr/share/licenses/dosh-linux-amd64/LICENSE"
-}
-
-package_dosh-linux-arm64() {
-	pkgdesc='Docker shell for linux/arm64 platform'
-	depends+=(dosh)
-
-	cd "dosh-$pkgver"
-	make DESTDIR="$pkgdir" PREFIX="/usr" install-linux-arm64-dosh
-	install -D -m 644 LICENSE "$pkgdir/usr/share/licenses/dosh-linux-arm64/LICENSE"
-}
-
-package_dosh-linux-arm() {
-	pkgdesc='Docker shell for linux/arm platform'
-	depends+=(dosh)
-
-	cd "dosh-$pkgver"
-	make DESTDIR="$pkgdir" PREFIX="/usr" install-linux-arm-dosh install-linux-arm-v6-dosh install-linux-arm-v7-dosh
-	install -D -m 644 LICENSE "$pkgdir/usr/share/licenses/dosh-linux-arm/LICENSE"
-}
-
-package_dosh-linux-ppc64le() {
-	pkgdesc='Docker shell for linux/ppc64le platform'
-	depends+=(dosh)
-
-	cd "dosh-$pkgver"
-	make DESTDIR="$pkgdir" PREFIX="/usr" install-linux-ppc64le-dosh
-	install -D -m 644 LICENSE "$pkgdir/usr/share/licenses/dosh-linux-ppc64le/LICENSE"
-}
-
-package_dosh-linux-riscv64() {
-	pkgdesc='Docker shell for linux/riscv64 platform'
-	depends+=(dosh)
-
-	cd "dosh-$pkgver"
-	make DESTDIR="$pkgdir" PREFIX="/usr" install-linux-riscv64-dosh
-	install -D -m 644 LICENSE "$pkgdir/usr/share/licenses/dosh-linux-riscv64/LICENSE"
-}
-
-package_dosh-linux-s390x() {
-	pkgdesc='Docker shell for linux/s390x platform'
-	depends+=(dosh)
-
-	cd "dosh-$pkgver"
-	make DESTDIR="$pkgdir" PREFIX="/usr" install-linux-s390x-dosh
-	install -D -m 644 LICENSE "$pkgdir/usr/share/licenses/dosh-linux-s390x/LICENSE"
+	make DESTDIR="$pkgdir" PREFIX="/usr" install-linux-platforms
+	install -D -m 644 LICENSE "$pkgdir/usr/share/licenses/dosh-linux-platforms/LICENSE"
 }
 
 package_dosh-posh() {