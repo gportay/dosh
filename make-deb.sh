9014988c0fdd2ed01a4e3b078c6e46c9ed6c5f49
Gaël PORTAY <gael.portay@gmail.com>
2026-03-07 14:17:29 +0100

    docker: move Dockerfile.deb to docker/deb/Dockerfile
    This moves Dockerfile to docker subdirectory.

--- make-deb.sh
+++ make-deb.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S DOSH_DOCKERFILE=Dockerfile.deb parent=1 dosh --no-doshrc
+#!/usr/bin/env -S DOSH_DOCKERFILE=docker/deb/Dockerfile parent=1 dosh --no-doshrc
 set -e
 dpkg-buildpackage -us -uc "$@"
 lintian ../dosh*.dsc ../dosh*.deb
