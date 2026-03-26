d1dd80b957328ff37133c69ee6c81967e65613a1
Gaël PORTAY <gael.portay@gmail.com>
2026-03-26 21:38:58 +0100

    Fix "make-pkg.sh: shellcheck PKGBUILD"
    This fixes commit 87e94aed436bbe55b1e39c438fe21e81c4136dfc.

--- make-pkg.sh
+++ make-pkg.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env -S DOSH_DOCKERFILE=docker/pkg/Dockerfile dosh --no-doshrc
 set -e
 makepkg --force --skipchecksums "$@"
-shellcheck --shell=bash --exclude=SC2034,SC2154,SC2164 PKGBUILD
+shellcheck --shell=bash --exclude=SC2034,SC2154,SC2164 PKGBUILD*
 namcap PKGBUILD* *.pkg.tar*
